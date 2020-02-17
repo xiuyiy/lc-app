6b5991802f71a3a19ac60bdea0fbc37b73ec3571
Ye, Xiuyi <xiye@microstrategy.com>
2020-02-17 12:04:44 -0500

    Updates

---
+++ precache-manifest.4e613d1fd3c8f6bed2732b6aff4b6ec3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0638e1f720fede72d048d343a0f0e65",
+    "url": "/lc-app/index.html"
+  },
+  {
+    "revision": "bad561613a6e3d7723fb",
+    "url": "/lc-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "53358d8190e70b76c62c",
+    "url": "/lc-app/static/js/2.b66724d9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/lc-app/static/js/2.b66724d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bad561613a6e3d7723fb",
+    "url": "/lc-app/static/js/main.a5494017.chunk.js"
+  },
+  {
+    "revision": "6283bd468e4309b62ea7",
+    "url": "/lc-app/static/js/runtime-main.8d2ca1b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lc-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
